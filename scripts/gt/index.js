bb0e3e3b571732a5f2ebed215eac04c1d2935a8e
vivaxy <xyxuye2007@126.com>
2017-08-26 10:36:04 +0800

    refactor(gt): :hammer:

--- scripts/gt/index.js
+++ scripts/gt/index.js
@@ -5,19 +5,14 @@
 
 const Listr = require('listr');
 
-let data;
-
 const sleep = (timeout) => {
     return new Promise((resolve) => {
         setTimeout(resolve, timeout);
     });
 };
 
-const copyFiles = () => {
-
-    const {
-        presets,
-    } = data;
+const copyFiles = (options) => {
+    const { presets } = options;
 
     const files = [
         'scripts/config.js',
@@ -32,26 +27,20 @@ const copyFiles = () => {
         'yarn.lock',
     ];
 
-    return sleep(1000).then(() => {
-        return presets.copyFiles(files);
-    });
+    return async() => {
+        await sleep(1000);
+        await presets.copyFiles(files);
+    };
 };
 
-const updatePackageJSON = () => {
-
-    const {
-        project,
-        scaffold,
-        presets,
-    } = data;
-
+const updatePackageJSON = (options) => {
+    const { project, presets } = options;
     const projectGit = project.git || {};
+    const filename = 'package.json';
 
-    const filename = `package.json`;
-
-    return sleep(1000).then(() => {
-        return presets.updateJson(filename, (data) => {
-
+    return async() => {
+        await sleep(1000);
+        await presets.updateJson(filename, (data) => {
             const {
                 name,
                 version,
@@ -71,7 +60,7 @@ const updatePackageJSON = () => {
 
             return {
                 name: project.name,
-                version: `0.0.0`,
+                version: '0.0.0',
                 reactScaffoldVersion: version,
                 description,
                 main,
@@ -91,78 +80,62 @@ const updatePackageJSON = () => {
                 peerDependencies,
             };
         });
-    });
-
+    };
 };
 
-const updateREADME = () => {
+const updateREADME = (options) => {
+    const { project, presets } = options;
+    const filename = 'README.md';
 
-    const {
-        project,
-        scaffold,
-        presets,
-    } = data;
-
-    const filename = `README.md`;
-
-    return sleep(1000).then(() => {
-        return presets.updateFile(filename, (data) => {
-            const projectData = data.split(`----------`)[1];
+    return async() => {
+        await sleep(1000);
+        await presets.updateFile(filename, (data) => {
+            const projectData = data.split('----------')[1];
             return projectData.replace(/__________/g, `${project.name}
 
 Initialized by [vivaxy/gt-front-end-scaffold](https://github.com/vivaxy/gt-front-end-scaffold)`);
         });
-    });
-
+    };
 };
 
-const updateCHANGELOG = () => {
+const updateCHANGELOG = (options) => {
+    const { presets } = options;
+    const filename = 'CHANGELOG.md';
 
-    const {
-        project,
-        scaffold,
-        presets,
-    } = data;
-
-    const filename = `CHANGELOG.md`;
-
-    return sleep(1000).then(() => {
-        return presets.updateFile(filename, (data) => {
+    return async() => {
+        await sleep(1000);
+        await presets.updateFile(filename, (data) => {
             return '';
         });
-    });
-
+    };
 };
 
-exports.init = (options) => {
-
-    data = options;
-
+export const init = (options) => {
     return new Listr([
         {
-            title: `copy files`,
-            task: copyFiles,
+            title: 'copy files',
+            task: copyFiles(options),
         },
         {
-            title: `update package.json`,
-            task: updatePackageJSON,
+            title: 'update package.json',
+            task: updatePackageJSON(options),
         },
         {
-            title: `update README.md`,
-            task: updateREADME,
+            title: 'update README.md',
+            task: updateREADME(options),
         },
         {
-            title: `update CHANGELOG.md`,
-            task: updateCHANGELOG,
+            title: 'update CHANGELOG.md',
+            task: updateCHANGELOG(options),
         },
     ]);
 
 };
 
-exports.after = () => {
+export const after = () => {
     console.log(`
         - run 'yarn install' to initialize the project
         - run 'yarn run dev' to start a development server
         - run 'yarn run build' to build release files
-`)
+`);
 };
