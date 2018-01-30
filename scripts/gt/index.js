10207542ebb05e4f88c61e6fec674137d7cc2c80
vivaxy <xyxuye2007@126.com>
2018-01-30 10:43:04 +0800

    fix(gt): :bug:Support prettier within gt scripts

--- scripts/gt/index.js
+++ scripts/gt/index.js
@@ -5,13 +5,13 @@
 
 const Listr = require('listr');
 
-const sleep = (timeout) => {
-    return new Promise((resolve) => {
+const sleep = timeout => {
+    return new Promise(resolve => {
         setTimeout(resolve, timeout);
     });
 };
 
-const copyFiles = (options) => {
+const copyFiles = options => {
     const { presets } = options;
 
     const files = [
@@ -21,121 +21,109 @@ const copyFiles = (options) => {
         '.babelrc',
         '.editorconfig',
         '.gitignore',
+        '.prettierignore',
         'CHANGELOG.md',
         'LICENSE',
         'webpack.config.js',
-        'yarn.lock',
+        'yarn.lock'
     ];
 
-    return async() => {
+    return async () => {
         await sleep(1000);
         await presets.copyFiles(files);
     };
 };
 
-const updatePackageJSON = (options) => {
+const updatePackageJSON = options => {
     const { project, presets } = options;
     const projectGit = project.git || {};
     const filename = 'package.json';
 
-    return async() => {
+    return async () => {
         await sleep(1000);
-        await presets.updateJson(filename, (data) => {
+        await presets.updateJson(filename, data => {
             const {
-                name,
+                // to remove
                 version,
-                description,
-                main,
-                scripts,
                 repository,
-                keywords,
-                author,
-                license,
-                bugs,
-                homepage,
-                dependencies,
-                devDependencies,
-                peerDependencies,
+
+                // not changed
+                ...rest
             } = data;
 
             return {
+                ...rest,
                 name: project.name,
                 version: '0.0.0',
                 reactScaffoldVersion: version,
-                description,
-                main,
-                scripts,
                 repository: {
                     type: repository.type,
-                    url: projectGit.repositoryURL,
+                    url: projectGit.repositoryURL
                 },
-                keywords,
-                author,
-                license,
                 bugs: {
-                    url: undefined,
+                    url: projectGit.repositoryURL
                 },
-                dependencies,
-                devDependencies,
-                peerDependencies,
+                homepage: projectGit.repositoryURL
             };
         });
     };
 };
 
-const updateREADME = (options) => {
+const updateREADME = options => {
     const { project, presets } = options;
     const filename = 'README.md';
 
-    return async() => {
+    return async () => {
         await sleep(1000);
-        await presets.updateFile(filename, (data) => {
+        await presets.updateFile(filename, data => {
             const projectData = data.split('----------')[1];
-            return projectData.replace(/__________/g, `${project.name}
+            return projectData.replace(
+                /__________/g,
+                `${project.name}
 
-Initialized by [vivaxy/gt-front-end-scaffold](https://github.com/vivaxy/gt-front-end-scaffold)`);
+Initialized by [vivaxy/gt-front-end-scaffold](https://github.com/vivaxy/gt-front-end-scaffold)`
+            );
         });
     };
 };
 
-const updateCHANGELOG = (options) => {
+const updateCHANGELOG = options => {
     const { presets } = options;
     const filename = 'CHANGELOG.md';
 
-    return async() => {
+    return async () => {
         await sleep(1000);
-        await presets.updateFile(filename, (data) => {
+        await presets.updateFile(filename, data => {
             return '';
         });
     };
 };
 
-export const init = (options) => {
+export const init = options => {
     return new Listr([
         {
             title: 'copy files',
-            task: copyFiles(options),
+            task: copyFiles(options)
         },
         {
             title: 'update package.json',
-            task: updatePackageJSON(options),
+            task: updatePackageJSON(options)
         },
         {
             title: 'update README.md',
-            task: updateREADME(options),
+            task: updateREADME(options)
         },
         {
             title: 'update CHANGELOG.md',
-            task: updateCHANGELOG(options),
-        },
+            task: updateCHANGELOG(options)
+        }
     ]);
-
 };
 
 export const after = () => {
     console.log(`
-        - run 'yarn install' to initialize the project
-        - run 'yarn run dev' to start a development server
-        - run 'yarn run build' to build release files
+        - Run 'yarn install' to initialize the project.
+        - Run 'yarn run dev' to start a development server.
+        - Run 'yarn run build' to build release files.
 `);
 };
